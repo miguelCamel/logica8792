3f62e60728037751e19b4d9764d27dd809a12f4a
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-09 20:38:01 -0300

    ContasBasicas

--- main.c
+++ main.c
@@ -4,8 +4,12 @@ int main(){
 
     int a = 5;
     int b = 4;
+    
 
-    printf("O maior numero é: %d\n", (a > b) ? a : b);
+    printf("O maior numero é: %d\n", (a + b));
+    printf("A subtração dos dois números é: %d\n", (a - b));
+    printf("A multiplicação dos dois números é: %d\n", (a * b));
+    printf("A divisão dos dois número é: %d\n", (a / b ));
 
     return 0;
 }
