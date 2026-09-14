235275a7f940e68a7a9815f829246dfe36a63e7e
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-14 19:26:20 -0300

    Ternario

--- main.c
+++ main.c
@@ -1,30 +1,16 @@
 #include<stdio.h>
 #include<windows.h>
-#include<string.h>
 
 int main(){
+SetConsoleOutputCP(65001);
+SetConsoleCP(65001);
+float n1, n2;
+int opcao;
+
+printf("%s", n > 0 ? "Positivo" : (n < 0 ? "Negativo" : "Zero"));
+    
 
 
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
-
-int n;
-char text[3] = "sim";
-char texto[3] = "nao";
-
-
-printf("Digite um número: ");
-scanf("%d", &n);
-
-printf("Você deseja saber o antecessor?");
-scanf("%d");
-if ("sim"){
-printf("Antecessor: %d\n", n - 1);
-
-}else{
-    printf("Ok, Boa noite!\n");
-
-}
 
 
 
