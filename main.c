11fde86417a9ae02037ed29941a75a06c927e246
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-14 20:22:18 -0300

    MaiorMenor corrigido

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ scanf("%d %d %d", &n1, &n2, &n3);
 
 maior = n1;
 
-if(n2 > maior) maior = n1;
-if(n3 > maior) maior = n2;
+if(n2 > maior) maior = n2;
+if(n3 > maior) maior = n3;
 
 printf("Maior: %d", maior);
 
