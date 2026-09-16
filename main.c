a561688e8575efaa85658bdabba3938dee79f574
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-16 19:08:47 -0300

    RoletaDaMorte

--- main.c
+++ main.c
@@ -4,45 +4,20 @@
 int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
-float a, b, c, d, e, maior, igual, menor;
+int idade;
+float altura;
 
-printf("Digite um número para a: ");
-scanf("%f", &a);
-printf("Digite um número para b: ");
-scanf("%f", &b);
-printf("Digite um número para c: ");
-scanf("%f", &c);
-printf("Digite um número para d: ");
-scanf("%f", &d);
-printf("Digite um número para e: ");
-scanf("%f", &e);
+printf("Bem vindos ao brinquedo roleta da morte!");
+printf("\nQual sua idade?: ");
+scanf("%d", &idade);
+printf("Qual sua altura?: ");
+scanf("%f", &altura);
 
-maior = a;
-igual = a;
-menor = a;
+if((idade >= 18) && (altura >= 1.60)){
+    printf("Você pode brincar na roleta!");
 
-if(b > maior) maior = b;
-if(c > maior) maior = c;
-if(d > maior) maior = d;
-if(e > maior) maior = e;
-
-if(b = b) igual = b;
-if(c = c) igual = c;
-if(d = d) igual = d;
-if(e = e) igual = e;
-
-if(b < menor) menor = b;
-if(c < menor) menor = c;
-if(d < menor) menor = d;
-if(e < menor) menor = e;
-
-
-printf("Maior: %.2f\n", maior);
-printf("Menor: %.2f\n", menor);
-
-if( (a == b == c == d == e) == igual);
-printf("Os cincos são iguais");
-
+}else{
+    printf("Você não pode brincar na roleta!");
+}
 return 0;
-
 }
