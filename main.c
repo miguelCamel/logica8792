7608afe5e34c03fe33e13cb5ebbe5e246be1538f
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-16 19:40:25 -0300

    ee

--- main.c
+++ main.c
@@ -5,19 +5,20 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 int idade;
-float altura;
+int ehEstudante;
 
-printf("Bem vindos ao brinquedo roleta da morte!");
-printf("\nQual sua idade?: ");
+printf("Qual sua idade?");
 scanf("%d", &idade);
-printf("\nQual sua altura?: ");
-scanf("%d", &altura);
+printf("\nVocê é estudando (0 - Não | 1 - Sim):");
+scanf("%d", &ehEstudante);
 
-if((idade >= 18) || (altura >= 1.60)){
-    printf("Você pode brincar na roleta!");
+if((idade >= 60) && (ehEstudante == 1)){
+    printf("Parabéns, você tem direito a desconto!");
 
 }else{
-    printf("Você não pode brincar");
+    printf("Infelizmente você não terá desconto!");
+
 }
 
+return 0;
 }
