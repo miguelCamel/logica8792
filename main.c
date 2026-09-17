0ad0cde1f1a78a77a9484cd801c0e684c15bbca6
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-16 21:58:59 -0300

    SwitchPedagio

--- main.c
+++ main.c
@@ -5,15 +5,22 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-float a, b, c;
+int opcao;
 
-printf("Digite os três lados do triângulo: ");
-scanf("%f %f %f", &a, &b, &c);
-
-if(a + b > c && a + c > b && b + c > a){
-    printf("Os lados formam um triângulo!\n");
-}else{
-    printf("Os lados NÂO formam um triângulo!\n");
+printf("--- SISTEMA DE PEDÁGIO---\n");
+printf("1 -Moto\n");
+printf("2 -Carro Passeio\n");
+scanf("%d", &opcao);
+switch(opcao){
+    case 1:
+    printf("Cateogira: Moto | Tarifa: R$ 5,00\n");
+    break;
+    case 2:
+    printf("Categoria: Carro Passeio | Tarifa: R$ 10,00\n");
+    break;
+    default:
+    printf("Erro: Categoria não considerada no sistema.\n");
+    break;
 }
 return 0;
 }
