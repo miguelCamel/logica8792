e169dec700958ed86605eade8c06d5c7b741dde8
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-18 20:11:43 -0300

    MesesSwitch1

--- main.c
+++ main.c
@@ -5,30 +5,64 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-int opcao;
+int mes;
+printf("Descubra a estação do ano de cada mês no Brasil\n");
+printf("Digite o nûmero do seu respectivo mês:  ");
+scanf("%d", &mes);
 
-printf("--- SISTEMA DE PEDÁGIO---\n");
-printf("1 -Moto\n");
-printf("2 -Carro Passeio\n");
-printf("3 - Caminhão\n");
-printf("4 - Ônibus\n");
-scanf("%d", &opcao);
-switch(opcao){
+switch(mes){
+
     case 1:
-    printf("Cateogira: Moto | Tarifa: R$ 5,00\n");
+    printf("Seu mês é: Janeiro\n");
+    printf("Sua estação é: Verão ");
     break;
     case 2:
-    printf("Categoria: Carro Passeio | Tarifa: R$ 10,00\n");
+    printf("Seu mês é: Fevereiro\n");
+    printf("Sua estação é: Verão");
     break;
     case 3:
-    printf("Categoria: Caminhão | Tarifa R$ 25,00\n");
+    printf("Seu mês é: Março\n");
+    printf("Sua estação é: Verão");
     break;
     case 4:
-    printf("Categoria: Ônibus | Tarifa: R$ 30,00\n");
+    printf("Seu mês é: Abril\n");
+    printf("Sua estação é: Outono");
+    break;
+    case 5:
+    printf("Seu mês é: Maio\n");
+    printf("Sua estação é: Outono");
+    break;
+    case 6:
+    printf("Seu mês é: Junho\n");
+    printf("Sua estação é: Outono");
+    break;
+    case 7:
+    printf("Seu mês é: Julho\n");
+    printf("Sua estação é: Inverno");
+    break;
+    case 8:
+    printf("Seu mês é: Agosto\n");
+    printf("Sua estação é: Inverno");
+    break;
+    case 9:
+    printf("Seu mês é: Setembro\n");
+    printf("Sua estação é: Inverno");
+    break;
+    case 10:
+    printf("Seu mês é: Outubro\n");
+    printf("Sua estação é: Primavera");
+    break;
+    case 11:
+    printf("Seu mês é: Novembro\n");
+    printf("Sua estação é: Primavera");
+    break;
+    case 12:
+    printf("Seu mês é: Dezembro\n");
+    printf("Sua estação é: Primavera");
     break;
     default:
-    printf("Erro: Categoria não considerada no sistema.\n");
-    break;
+    printf("Digite um valor valido");
+    
 }
 return 0;
 }
--- secondary.c
+++ secondary.c
@@ -1,20 +1,41 @@
 #include<stdio.h>
+#include<string.h>
 #include<windows.h>
-
+ 
 int main(){
-
-SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
+SetConsoleCP(65001);
+char resposta[3];
 
-int n;
+printf("Bem vindo ao quiz!!!\n");
+printf("1-Você esta fazendo um trabalho em grupo e todo mundo começa a discutir. O que voce faria?\n");
+printf("[A] Tentaria entender por que cada pessoa esta chateada\n");
+printf("[B] Tentaria mostrar os pontos bons e ruins de cada ideia\n");
+printf("[C] Verificaria se alguém está passando mal ou precisa de ajuda\n");
+printf("[D] Tentaria explicar tudo novamente para o grupo\n");
+printf("[E] Pensaria em uma forma mais bonita de apresentar o trabalho\n");
+printf("[F] Tentaria descobrir o que está dando de errado e consertar\n");
+printf("Digite a letra de sua resposta: ");
+scanf("%c", &resposta);
 
-printf("Digite um número: ");
-scanf("%d", &n);
+if((resposta == 'a') || (resposta == 'A')){
+    printf("Voce se encaixa como: Psicologo");
+}else if((resposta == 'b') || (resposta == 'B')){
+    printf("Voce se encaixa como: Advogado");
+}else if((resposta == 'c') || (resposta == 'C')){
+    printf("Voce se encaixa como: Medico");
+}else if((resposta == 'd') || (resposta == 'D')){
+    printf("Voce se encaixa como: Professor");
+}else if((resposta == 'e') || (resposta == 'E')){
+    printf("Voce se encaixa como: Designer");
+}else if((resposta == 'f') || (resposta == 'F')){
+    printf("Voce se encaixa como: Programador");
+}else{
+    printf("Digite uma alternativa válida");
+}
 
-if(n > 0){
-    printf("Positivo\n");
-}else
- printf("Negativo\n");
+
+
 
 
 
