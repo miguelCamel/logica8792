11b7e939174a0c484d2a3412ce457a22421396b2
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-18 21:17:14 -0300

    AnosBissextosIfEElse

--- main.c
+++ main.c
@@ -5,19 +5,16 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-int n;
-printf("Digite um número: ");
-scanf("%d", &n);
+int ano;
+printf("Digite o ano: ");
+scanf("%d", &ano);
 
-if(n % 3 == 0 && n % 5 == 0){
-    printf("Múltiplo de 3 e 5\n");
-}else if(n % 3 == 0){
-    printf("Multiplo de 3\n");
-
-}else if(n % 5 == 0){
-    printf("Multiplo de 5\n");
+if((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0){
+    printf("Ano: %d\n", ano);
+    printf("Ano: Bissexto!\n");
 }else{
-    printf("Não é múltiplo de 3 nem de 5\n");
+    printf("Ano: %d\n", ano);
+    printf("Ano NÃO bissexto!\n");
 }
 return 0;
 }
