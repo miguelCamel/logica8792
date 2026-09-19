583317785ab4de9aa2ca7f66f098721a70bc9bb2
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-18 21:57:14 -0300

    For

--- main.c
+++ main.c
@@ -5,23 +5,8 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-float peso, altura, imc;
-printf("Digite o peso(kg): ");
-scanf("%f", &peso);
-
-printf("Digite a altura(m): ");
-scanf("%f", &altura);
-
-imc = peso / (altura * altura);
-
-if(imc < 18.5){
-    printf("Classificação: Abaixo do peso!\n");
-}else if(imc < 25){
-    printf("Classificação: Peso normal!\n");
-}else if(imc < 30){
-    printf("Classificação: Sobrepreso!");
-}else{
-    printf("Classificação: Obesidade!");
+for(int i = 1; i <= 100; i++){
+    printf("%d\n", i);
 }
 return 0;
 }
