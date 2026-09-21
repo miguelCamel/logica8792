e943e38d3db6a35aa4aa9879759ed98f83ef5d23
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 20:42:52 -0300

    EscolhaArray

--- main.c
+++ main.c
@@ -7,19 +7,26 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-char nomes[4][20] ={"Aurea", "Miguel", "Julio", "Rafaela"};
-
-printf("%s\n", nomes[0]);
-printf("%s\n", nomes[1]);
-printf("%s\n", nomes[2]);
-printf("%s\n", nomes[3]);
+int numerosInteiros[5] = {1, 5, 10, 15, 20};
+float numerosFloats[5] = {10.5, 20.8, 30.2, 30.9, 50.1};
+char carros[3][20] = {"Fusca", "Camaro", "Ferrari"};
 
 
+printf("%d\n", numerosInteiros[0]);
+printf("%d\n", numerosInteiros[1]);
+printf("%d\n", numerosInteiros[2]);
+printf("%d\n", numerosInteiros[3]);
+printf("%d\n", numerosInteiros[4]);
 
+printf("%.2f\n", numerosFloats[0]);
+printf("%.2f\n", numerosFloats[1]);
+printf("%.2f\n", numerosFloats[2]);
+printf("%.2f\n", numerosFloats[3]);
+printf("%.2f\n", numerosFloats[4]);
 
-
-
-
+printf("%s\n", carros[0]);
+printf("%s\n", carros[1]);
+printf("%s\n", carros[2]);
 
 
     return 0;
