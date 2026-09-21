e314eb8066357aeedf9f7fc763f32304fb42a1aa
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 20:05:39 -0300

    NomesAura

--- main.c
+++ main.c
@@ -7,11 +7,12 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-char frutas[3][20] ={"Maçã", "Banana", "Laranja"};
+char nomes[4][20] ={"Aurea", "Miguel", "Julio", "Rafaela"};
 
-printf("%s\n", frutas[0]);
-printf("%s\n", frutas[1]);
-printf("%s\n", frutas[2]);
+printf("%s\n", nomes[0]);
+printf("%s\n", nomes[1]);
+printf("%s\n", nomes[2]);
+printf("%s\n", nomes[3]);
 
 
 
