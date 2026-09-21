63a71168ce7ba2ab7ae7a09f34f815cd3b8e02b3
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 19:49:29 -0300

    numaumentandofor

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-for(int i = 0; i < 10000; i++){
-    printf("%s\n", "Te amo");
-    i++;
+for(int i = 0; i < 10; i++){
+    printf("%s\n %d", "\nOs números estão aumentando: --->>", i);
+    
 }
 printf("\nMeu For funciounou!!!");
 return 0;
