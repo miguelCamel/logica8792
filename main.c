f7b97565e220688172fc152b40a8c52467de5adc
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 19:43:36 -0300

    ForFo

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<windows.h>
+#include<string.h>
 
 int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-for(int i = 1; i <= 100; i++){
-    printf("%d\n", i);
+for(int i = 0; i < 10000; i++){
+    printf("%s\n", "Te amo");
+    i++;
 }
+printf("\nMeu For funciounou!!!");
 return 0;
 }
