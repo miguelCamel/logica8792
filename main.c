9fdf708adbbc1b670045afd48e78fea866fb2ed0
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 21:11:09 -0300

    For2em2Até10

--- main.c
+++ main.c
@@ -7,9 +7,8 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-int idade[3] = {15, 56, 64};
-printf("%s %d %s", "Miguel tem, ", idade[0], "anos!\n");
-printf("%s %d %s", "Áurea tem, ", idade[1], "anos!\n");
-printf("%s %d %s", "Julio tem, ", idade[2], "anos!\n");
+for(int i = 0; i <= 10; i = i + 2){
+    printf("%d\n", i);
+}
     return 0;
 }
