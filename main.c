0309db27606c95ab12ef619d8f79b6e2570a97cf
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 21:29:59 -0300

    Somafor

--- main.c
+++ main.c
@@ -7,8 +7,17 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-for(int i = 10; i >= 0; i = i - 2){
-    printf("%d\n", i);
+int soma = 0;
+int i;
+
+for(i = 1; i <= 5; i++){
+    //0 + 1
+    // 1 + 2
+    //3 + 3
+    //6 + 4
+    //10 + 5
+    soma = soma + i;
 }
+printf("Soma é %d", soma);
     return 0;
 }
