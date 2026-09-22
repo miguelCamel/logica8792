70323a3da3aa7699dd9a9afed6f0a9d06112e886
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 21:00:55 -0300

    NomesArray

--- main.c
+++ main.c
@@ -7,27 +7,9 @@ int main(){
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-int numerosInteiros[5] = {1, 5, 10, 15, 20};
-float numerosFloats[5] = {10.5, 20.8, 30.2, 30.9, 50.1};
-char carros[3][20] = {"Fusca", "Camaro", "Ferrari"};
-
-
-printf("%d\n", numerosInteiros[0]);
-printf("%d\n", numerosInteiros[1]);
-printf("%d\n", numerosInteiros[2]);
-printf("%d\n", numerosInteiros[3]);
-printf("%d\n", numerosInteiros[4]);
-
-printf("%.2f\n", numerosFloats[0]);
-printf("%.2f\n", numerosFloats[1]);
-printf("%.2f\n", numerosFloats[2]);
-printf("%.2f\n", numerosFloats[3]);
-printf("%.2f\n", numerosFloats[4]);
-
-printf("%s\n", carros[0]);
-printf("%s\n", carros[1]);
-printf("%s\n", carros[2]);
-
-
+int idade[3] = {15, 56, 64};
+printf("%s %d %s", "Miguel tem, ", idade[0], "anos!\n");
+printf("%s %d %s", "Áurea tem, ", idade[1], "anos!\n");
+printf("%s %d %s", "Julio tem, ", idade[2], "anos!\n");
     return 0;
 }
