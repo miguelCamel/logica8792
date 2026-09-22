3598b7bf623a8e4fbb289e48f15ad38bcf804735
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 21:39:55 -0300

    Array

--- main.c
+++ main.c
@@ -6,18 +6,9 @@ int main(){
 
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
+int numero[] = {1, 2, 3};
+numero[0] = 10;
 
-int soma = 0;
-int i;
-
-for(i = 1; i <= 5; i++){
-    //0 + 1
-    // 1 + 2
-    //3 + 3
-    //6 + 4
-    //10 + 5
-    soma = soma + i;
-}
-printf("Soma é %d", soma);
+printf("%d", numero[0]);
     return 0;
 }
