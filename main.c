5ab3645dc7a2621a90680d08f3694826fe8936fc
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-21 21:44:46 -0300

    Array2

--- main.c
+++ main.c
@@ -6,9 +6,11 @@ int main(){
 
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
-int numero[] = {1, 2, 3};
+int numero[3];
 numero[0] = 10;
+numero[1] = 20;
+numero[2] = 30;
 
-printf("%d", numero[0]);
+printf("%d", numero[2]);
     return 0;
 }
