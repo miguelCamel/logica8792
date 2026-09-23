fbf4df609a59f769300f0bfce69ecff758dff691
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-23 19:32:14 -0300

    Fatorial

--- main.c
+++ main.c
@@ -2,13 +2,31 @@
 #include<windows.h>
 #include<string.h>
 
+
+int fatorial(int n){
+    int resultado = 1;
+    for(int i = 1; i <= n; i++){
+        resultado *=i; //resultado = resultado * i;
+    }
+    return resultado;
+}
+
+
+
 int main(){
 
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
-int numeros[] = {10,20, 30};
-int lenght = sizeof(numeros) / sizeof(numeros[0]);
+int numero = 5;
+printf("Fatorial de %d = %d\n", numero, fatorial(numero));
 
-printf("%d", lenght);
+
+
+
+
+
+
+
+
     return 0;
 }
