a3ab3209f05670d352e87957b7edd6021728da3b
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-23 21:29:47 -0300

    contarVogais

--- main.c
+++ main.c
@@ -2,19 +2,28 @@
 #include<windows.h>
 #include<string.h>
 
-void linha(int tamanho){
-  for(int i = 0; i < tamanho; i++){
-    printf("*");
+void contarVogais(char palavra[]){
+    int contador = 0;
+  for(int i = 0; i <strlen(palavra); i++){
+    char c = palavra[i];
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+        contador++;
   }
-  printf("\n");
-}
 
+}
+printf("A palavra '%s' tem %d vogais.\n", palavra, contador);
+}
 int main(){
 
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
 
-linha(2000);
+char resposta[20];
+
+printf("Digite uma palavra: ");
+scanf("%s", resposta);
+
+contarVogais(resposta);
 
     return 0;
 }
