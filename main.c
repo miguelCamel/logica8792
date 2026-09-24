d2240acc5eaa8eaa5cf33efe971dfc6b938707e6
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-24 19:12:10 -0300

    MenorMaiorWhile

--- main.c
+++ main.c
@@ -1,25 +1,15 @@
 #include<stdio.h>
 #include<windows.h>
-#include<string.h>
-
-
-
 
 int main(){
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
 
-SetConsoleOutputCP(65001);
-SetConsoleCP(65001);
+    int contador = 10;
 
-int numero;
-
-printf("Digite um número: ");
-scanf("%d", &numero);
-
-if(numero % 2 == 0 ){
-    printf("Seu número é par");
-}else{
-    printf("Seu número é impar");
-}
-
+    while(contador >= 1){
+        printf("%d\n", contador);
+        contador--;
+    }
     return 0;
 }
