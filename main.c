bf008c462ce647f914d166c2ab56cabc23fbd1a8
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-23 21:41:10 -0300

    MaxMin

--- main.c
+++ main.c
@@ -2,28 +2,25 @@
 #include<windows.h>
 #include<string.h>
 
-void contarVogais(char palavra[]){
-    int contador = 0;
-  for(int i = 0; i <strlen(palavra); i++){
-    char c = palavra[i];
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-        contador++;
-  }
+void maxMin(int v[], int tamanho){
+    int max = v[0], min = v[0];
+    for(int i = 1; i < tamanho; i++){
+    if(v[i] > max) max = v[i];
+    if(v[i] < min) min = v[i];
+}
+printf("Máximo: %d, Mínimo: %d\n", max, min);
+}
 
-}
-printf("A palavra '%s' tem %d vogais.\n", palavra, contador);
-}
+
+
+
 int main(){
 
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
+int numero[] = {4, 8, 2, 15, 6};
+maxMin(numero, 5);
 
-char resposta[20];
-
-printf("Digite uma palavra: ");
-scanf("%s", resposta);
-
-contarVogais(resposta);
 
     return 0;
 }
