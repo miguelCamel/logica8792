68da4215bbe4a7018280d5bbcc8e61a36c9dd30f
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-23 21:56:23 -0300

    ParImpar

--- main.c
+++ main.c
@@ -2,15 +2,6 @@
 #include<windows.h>
 #include<string.h>
 
-void maxMin(int v[], int tamanho){
-    int max = v[0], min = v[0];
-    for(int i = 1; i < tamanho; i++){
-    if(v[i] > max) max = v[i];
-    if(v[i] < min) min = v[i];
-}
-printf("Máximo: %d, Mínimo: %d\n", max, min);
-}
-
 
 
 
@@ -18,9 +9,17 @@ int main(){
 
 SetConsoleOutputCP(65001);
 SetConsoleCP(65001);
-int numero[] = {4, 8, 2, 15, 6};
-maxMin(numero, 5);
 
+int numero;
+
+printf("Digite um número: ");
+scanf("%d", &numero);
+
+if(numero % 2 == 0 ){
+    printf("Seu número é par");
+}else{
+    printf("Seu número é impar");
+}
 
     return 0;
 }
