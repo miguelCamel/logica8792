d3ad137112575cb28ef6e91fadb233ceeb9a9477
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-24 19:19:24 -0300

    Sistema de Repetição while

--- main.c
+++ main.c
@@ -5,11 +5,15 @@ int main(){
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    int contador = 10;
+    int numero;
+    printf("Digite um número positivo: ");
+    scanf("%d", &numero);
 
-    while(contador >= 1){
-        printf("%d\n", contador);
-        contador--;
+    while(numero <= 0){
+        printf("Número inválido! Digite novamente: ");
+        scanf("%d", &numero);
     }
+    printf("Número válido: %d\n", numero);
+    
     return 0;
 }
