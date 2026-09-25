f5126700981eec3feab5ef8d656bea0c89fa38e7
miguelCamel <miguelreispaim2010@gmail.com>
2026-09-25 19:45:59 -0300

    EhPrimo?FunçaoFor

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 #include<windows.h>
 
+int ehPrimo(int n){
+    if(n < 2)return 0;
+    for(int i = 2; i < n; i++){
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
 int main(){
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
-
-int numero, contador = 0;
-
-printf("Digite números (0 para parar): \n");
-scanf("%d", &numero);
-
-while(numero != 0){
-    contador++;
+    int numero;
+    printf("Digite um número: ");
     scanf("%d", &numero);
-}
-printf("Quantidade de número digitados: %d\n", contador);
+    if(ehPrimo(numero))
+        printf("%d é primo\n", numero);
+    else
+    printf("%d não é primo\n", numero);
     return 0;
 }
